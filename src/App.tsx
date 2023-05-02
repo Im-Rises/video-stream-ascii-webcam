62b210fbe41b2370255e8e2771065217a3e3e89f
Im-Rises <quentin-morel88@hotmail.com>
2023-05-02 13:12:11 -0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import {GITHUB_LINK_TEXT, GITHUB_URL} from './constants/pixel-ascii';
 
 const App = () => (
 	<div className='App'>
-		<GitHubProjectPanel link={GITHUB_URL}
-			linkText={GITHUB_LINK_TEXT}/>
+		{/* <GitHubProjectPanel link={GITHUB_URL} */}
+		{/*	linkText={GITHUB_LINK_TEXT}/> */}
 		<CameraAsciiPanel/>
 	</div>
 );
